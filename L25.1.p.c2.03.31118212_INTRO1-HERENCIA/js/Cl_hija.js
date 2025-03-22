c05ac91ac97cb383c2831e5af599ea83744b56f5
MiguelTorresDT <1001.31118212.ucla@gmail.com>
2025-03-22 00:15:02 -0400

    1

---
+++ L25.1.p.c2.03.31118212_INTRO1-HERENCIA/js/Cl_hija.js
@@ -0,0 +1,8 @@
+import padre from "./Cl_padre.js";
+
+export default class hija {
+    constructor(atriA, atribB) {
+        super(atriA);
+        this.atribB = atribB;
+    }
+}
---
+++ L25.1.p.c2.03.31118212_INTRO1-HERENCIA/js/principal.js
@@ -0,0 +1,8 @@
+/**Crea una clase base, llamada Cl_padre, que contenga sólo un atributo llamado atribA. Luego,
+crea una clase hija llamada Cl_hija que herede de Cl_padre y que también contenga el atributo
+atribB. En el main creas una instancia de Cl_hija con los valores atribA=1 y atribA=2.
+ */
+
+import hija from "./Cl_hija.js";
+
+let hija1 = new hija(1, 2);
